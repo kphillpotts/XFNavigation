60653284e2bf6478293325f3642bf5c3d76712d7
Kym Phillpotts <kphillpotts@gmail.com>
2015-05-28 21:35:24 +1000

    Missed a file :)

---
+++ XFNavigation/XFNavigation/XFNavigation/Views/MasterDetail/SimpleMasterDetail/SimpleMasterDetailContainer.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace XFNavigation.Views.MasterDetail.SimpleMasterDetail
+{
+    public class SimpleMasterDetailContainer : MasterDetailPage
+    {
+        public SimpleMasterDetailContainer()
+        {
+            Master = new SimpleMasterPage();
+            Detail = new NavigationPage(new SimpleDetailPage());
+            this.MasterBehavior = MasterBehavior.Default;
+        }
+    }
+}
